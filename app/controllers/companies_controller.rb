7896a69335a561baeedcb77f2830200726a87ff3
unknown <Insead@.(none)>
2008-06-25 00:01:08 +0200

    Added company edit feature

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,6 +12,11 @@ class CompaniesController < ApplicationController
     @company = Company.new
   end
 
+  def edit
+    @company = Company.find(params[:id])
+	flash[:ticker] = @company.ticker
+  end
+  
   def create
     @company = Company.new(params[:company])
     respond_to do |format|
@@ -20,6 +25,21 @@ class CompaniesController < ApplicationController
 	    format.html { render :action => 'show' }
 	  else
 	    format.html { render :action => 'new' }
+	  end
+    end
+  end
+
+  def update
+	@company = Company.find(params[:id])
+    @company.ticker = flash[:ticker] 
+	@company.update_attributes(params[:company])
+	
+	respond_to do |format|
+      if @company.save
+  	    flash[:notice] = "Thanks for updating the details!!"
+	    format.html { render :action => 'show' }
+	  else
+	    format.html { render :action => 'edit' }
 	  end
     end
   end
