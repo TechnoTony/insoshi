4bad7c8db9612fa6de484f0fb07a675681aebaeb
unknown <Insead@.(none)>
2008-06-25 15:27:43 +0200

    Added (inactive) admin filter to market

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -1,4 +1,6 @@
 class MarketController < ApplicationController
+# Add the code to require admin access, otherwise password can be compromised
+#  before_filter :login_required, :admin_required
 
   def index
     @fxtp_cmd = 'list_keywords'
@@ -8,6 +10,7 @@ class MarketController < ApplicationController
   
   def test_mkt
     if params[:admin]
+	#this first line should probably be removed before deployment for security of password
 	  @fxtp_cmd = params[:cmd] + FORESIGHT_PWD + params[:args]	
 	else
 	  @fxtp_cmd = params[:cmd] + params[:args]
